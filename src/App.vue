ac96bff848284af90566a16bfa8a84ddd5a02443
TelesBruno <brunooliveirateles@gmail.com>
2021-11-10 14:09:37 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <button v-if="finalizouBusca" id="retornar" @click="finalizouBusca = false;palavra = '';">retornar</button>
+    <div v-if="!finalizouBusca" id="primeira-tela">
+      <div id="titulo">
+        <h2>Dicionário da Língua Portuguesa</h2>
+      </div>
+      <div id="pesquisar">
+        <input type="text" v-model="palavra" name="palavra" id="palavra" placeholder="pesquise uma palavra" />
+        <button @click="pesquisarPlavra()">Pesquisar</button>
+      </div>
+    </div>
+    <resultado
+    v-if="finalizouBusca"
+      :tipo="tipo"
+      :etimologia="etimologia"
+      :significado="significado"
+    ></resultado>
+  </div>
+</template>
+
+<script>
+import Resultado from './components/Resultado.vue'
+export default {
+  components: { Resultado },
+  name: 'app',
+  data () {
+    return {
+      palavra: '',
+      tipo: '',
+      etimologia: '',
+      significado: '',
+      finalizouBusca: false
+    }
+  },
+  methods: {
+    pesquisarPlavra: function () {
+      let api = 'https://significado.herokuapp.com/'
+      if (this.palavra === null || this.palavra === '') {
+        return
+      }
+      fetch((api += this.palavra))
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data[0].class)
+          console.log(data[0].etymology)
+          console.log(data[0].meanings[0])
+          this.tipo = data[0].class
+          this.etimologia = data[0].etymology
+          this.significado = data[0].meanings[0]
+          this.finalizouBusca = true
+        })
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  width: 360px;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #b2b5b8;
+}
+ 
+#pesquisar {
+  display: flex;
+}
+#retornar{
+  float:right;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+button {
+  border: 1px solid #3079ed;
+  margin-left: 10px;
+  background-color: #4d90fe;
+  color: white;
+}
+</style>
---
+++ src/components/Resultado.vue
@@ -0,0 +1,32 @@
+<template>
+<div id='resultado'>
+    <p><i>{{tipo}}</i></p>
+    <p><b>Significado:</b> <span>{{significado}}</span></p>
+    <p><b>Etimologia:</b><span>{{etimologia}}</span></p>
+</div>
+
+</template>
+
+<script>
+
+export default ({
+  name: 'resultado',
+  props: {
+    tipo: String,
+    etimologia: String,
+    significado: String
+  },
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+#resultado {
+  margin-top:30px;
+  color: black;
+}
+</style>
